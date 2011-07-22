31dc14d147f827510e7ebfccf7248be8e215ada5
Shamil CM <shamil.cm@gmail.com>
2011-07-22 20:36:57 +0530

    Removed conent slider code

--- index.php
+++ index.php
@@ -2,19 +2,11 @@
 <html>
 
 	<head>
-		<link href="styles/contentslider.css"  type="text/css" rel="stylesheet" />
+
 		<link href = 'styles/wrapper.css' type='text/css' rel='stylesheet' />
 		<script type="text/javascript" src="js/jquery.min.js"></script>
 <!--		<script type="text/javascript" src="https://apis.google.com/js/plusone.js"></script>-->
-		<script type="text/javascript" src="js/contentslider.js">
-
-		/***********************************************
-		* Featured Content Slider- (c) Dynamic Drive DHTML code library (www.dynamicdrive.com)
-		* This notice MUST stay intact for legal use
-		* Visit Dynamic Drive at http://www.dynamicdrive.com/ for this script and 100s more
-		***********************************************/
-
-		</script>
+		
 		<script type="text/javascript" src="js/script.js">
 		</script>
 		<title>
@@ -24,57 +16,7 @@
 	<body>
 
 	<div id = "wrapper">
-		<div id="paginate-slider1" class="pagination" > 
-<div class="col960"> 
- 
-<a href="#" class="toc">&nbsp;</a> 
-<a href="#" class="toc">&nbsp;</a> 
-<a href="#" class="toc">&nbsp;</a> 
-<!-- <a href="#" class="prev">&nbsp;</a> 
-<a href="#" class="next">&nbsp;</a> -->
-</div> 
-</div> 
-		 <div id="slider1" class="sliderwrapper"> 
-		<div class="contentdiv" style="background-image:url(images/1.jpg);"> 
-		<div class="col960"> 
-		<div class="slide1text"> 
-		<p class="link"><a href="marketing.php">Golf Club Websites</a></p> 
-		</div> 
-		</div></div> 
-		 
-		<div class="contentdiv" style="background-image:url(images/2.jpg);"> 
-		<div class="col960"> 
-		<div class="slide2text"> 
-		<p class="link"><a href="golf_course_photography.php">Photography</a></p> 
-		</div> 
-		</div> 
-		</div> 
-		 
-		<div class="contentdiv" style="background-image:url(images/logos.gif);"><div class="col960"> 
-		<div class="slide3text"><p>Re-establish and improve your <br /> 
-		corporate identity.</p> 
-		<p class="link"><a href="marketing_branding.php">Corporate Identity</a></p></div> 
-		</div></div> 
-		 
-		 
-		</div> 
-		 
-		 
-		<script type="text/javascript"> 
-		featuredcontentslider.init({
-			id: "slider1",  //id of main slider DIV
-			contentsource: ["inline", ""],  //Valid values: ["inline", ""] or ["ajax", "path_to_file"]
-			toc: "markup",  //Valid values: "#increment", "markup", ["label1", "label2", etc]
-			nextprev: ["Previous", "Next"],  //labels for "prev" and "next" links. Set to "" to hide.
-			revealtype: "click", //Behavior of pagination links to reveal the slides: "click" or "mouseover"
-			enablefade: [true, 0.155],  //[true/false, fadedegree]
-			autorotate: [true, 5500],  //[true/false, pausetime]
-			onChange: function(previndex, curindex){  //event handler fired whenever script changes slide
-				//previndex holds index of last slide viewed b4 current (1=1st slide, 2nd=2nd etc)
-				//curindex holds index of currently shown slide (1=1st slide, 2nd=2nd etc)
-			}
-		})
-		</script> 
+	
  
  	
   		
