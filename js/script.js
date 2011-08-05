c9b94203725cb4eb417664f72a90fc826ad07df7
Jaseem Abid <jaseemabid@gmail.com>
2011-08-06 04:00:17 +0530

    Auto hiding the drop down menu when one item from the list is clicked. Users can see the ajax loaded data just after they click now, dont have to take the mouse outside the dropdown to hide it

--- js/script.js
+++ js/script.js
@@ -1,29 +1,33 @@
 /* General Stuff */
 $(document).ready(function () {
-    var $oe_menu = $('#oe_menu');
-    var $oe_menu_items = $oe_menu.children('li');
-    var $oe_overlay = $('#oe_overlay');
-    $oe_menu_items.bind('mouseenter', function () {
-        var $this = $(this);
-        $this.addClass('slided selected');
-        $this.children('div').css('z-index', '9999').stop(true, true).slideDown(200, function () {
-            $oe_menu_items.not('.slided').children('div').hide();
-            $this.removeClass('slided');
-        });
-    }).bind('mouseleave', function () {
-        var $this = $(this);
-        $this.removeClass('selected').children('div').css('z-index', '1');
-    });
+	var $oe_menu = $('#oe_menu');
+	var $oe_menu_link = $('#oe_menu li ul li a');
+	var $oe_menu_items = $oe_menu.children('li');
+	var $oe_overlay = $('#oe_overlay');
+	$oe_menu_link.click(function(){
+		$(this).parent().parent().parent().fadeOut(300);
+	});
+	$oe_menu_items.mouseenter(function () {
+		var that = $(this);
+		that.addClass('slided selected');
+		that.children('div').css('z-index', '9999').stop(true, true).slideDown(200, function () {
+			$oe_menu_items.not('.slided').children('div').hide();
+			that.removeClass('slided');
+		});
+	}).mouseleave(function () {
+		var that = $(this);
+		that.removeClass('selected').children('div').css('z-index', '1');
+	});
 
-    $oe_menu.bind('mouseenter', function () {
-        var $this = $(this);
-        $oe_overlay.stop(true, true).fadeTo(200, 0.6);
-        $this.addClass('hovered');
-    }).bind('mouseleave', function () {
-        var $this = $(this);
-        $this.removeClass('hovered');
-        $oe_overlay.stop(true, true).fadeTo(200, 0);
-        $oe_menu_items.children('div').hide();
-    })
+	$oe_menu.mouseenter(function () {
+		var that = $(this);
+		$oe_overlay.stop(true, true).fadeTo(200, 0.6);
+		that.addClass('hovered');
+	}).mouseleave(function () {
+		var that = $(this);
+		that.removeClass('hovered');
+		$oe_overlay.stop(true, true).fadeTo(200, 0);
+		$oe_menu_items.children('div').hide();
+	})
 
 });
