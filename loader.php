1b7d38660561abf42f6eff0ac5132a821b43c3d6
Shamil CM <shamil.cm@gmail.com>
2011-08-25 04:17:41 +0530

    registration form css and html

--- loader.php
+++ loader.php
@@ -1,5 +1,54 @@
 <?php
 require_once 'config.php'; 
+
+if($_GET['page']=="register")
+{
+echo '<div id="stylized" class="myform">
+<form id="form" name="form" method="post" action="register.php">
+<h1>Registration</h1>
+<p>Fill in all the required details</p>
+
+<label>Name
+<span class="small">Add your name</span>
+</label>
+<input type="text" name="name" id="name" />
+
+<label>Email
+<span class="small">Add a valid address</span>
+</label>
+<input type="text" name="email" id="email" />
+
+<label>Name
+<span class="small">Add your name</span>
+</label>
+<input type="text" name="name" id="name" />
+
+<label>Password
+<span class="small">Min. size 6 chars</span>
+</label>
+<input type="password" name="password" id="password" />
+
+<label>Confirm Password
+<span class="small">Re-type your password</span>
+</label>
+<input type="password" name="password" id="password" />
+
+<label>Institution Name
+<span class="small">Add your institution</span>
+</label>
+<input type="text" name="password" id="password" />
+
+
+<button type="submit">Register</button>
+<div class="spacer"></div>
+
+</form>
+</div>';
+
+}
+
+else
+{
 $con = mysql_connect($host, $db_user, $db_password);
 if (!$con) {
 	die('Could not connect: ' . mysql_error());
@@ -12,4 +61,6 @@ if (!($row))
 	echo '404 Page Not Found';
 else
 	echo $row['article_content'];
+}
+
 ?>
