e64550827982b882d476e47d34a75638d80211fb
Pranav Ashok <pranavashok@gmail.com>
2011-08-25 00:04:00 +0530

    Ajax crawling implemented

--- index.php
+++ index.php
@@ -1,5 +1,4 @@
 <html>
-
 	<head>
 		<link rel="icon" type="image/png" href="styles/images/favicon.png" />
 		<link href = 'styles/wrapper.css' type='text/css' rel='stylesheet' />
@@ -9,30 +8,31 @@
 		
 		<link href="styles/jquery.mCustomScrollbar.css" rel="stylesheet" type="text/css" />
 		<link href="styles/coin-slider-styles.css" rel="stylesheet" type="text/css" />
-<link href='http://fonts.googleapis.com/css?family=Numans' rel='stylesheet' type='text/css'>
+		<link href='http://fonts.googleapis.com/css?family=Numans' rel='stylesheet' type='text/css'>
 
 
 		<script type="text/javascript" src="js/jquery.min.js"></script>
 		<script type="text/javascript" src="js/jquery-ui.min.js"></script>
-   <!-- <script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8/jquery-ui.min.js"></script>-->
-    <script type="text/javascript" src="js/jquery.mousewheel.min.js"></script>
+<!-- 		<script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8/jquery-ui.min.js"></script>-->
+    		<script type="text/javascript" src="js/jquery.mousewheel.min.js"></script>
 		<script type="text/javascript" src="js/jquery.easing.1.3.js"></script>
 		<script type="text/javascript" src="js/easySlider1.7.js"></script>
 <!--		<script type="text/javascript" src="https://apis.google.com/js/plusone.js"></script>-->
-<script type="text/javascript" src="js/jquery.history.js"></script>
+		<script type="text/javascript" src="js/jquery.history.js"></script>
 		<script type="text/javascript" src="js/jquery.lavalamp.1.2.js"></script>
 		<script  type="text/javascript" src="js/jquery.mCustomScrollbar.js"></script>		
 		<script type="text/javascript" src="js/script.js"></script>
 		<script type="text/javascript" src="js/coin-slider.js"></script>
 		<script type="text/javascript" src="js/jquery.tipsy.js"></script>
-		<script type="text/javascript"> 
-	
+		<script type="text/javascript">	
 	        $(document).ready(function () {
+			var _escaped_fragment_ = document.location.search;
+			if(!_escaped_fragment_) $("#innerpage").hide();
  			$('.sbicon').tipsy({gravity: 's'});
                         $.history.init(pageload);	
 	                $('a[href=' + window.location.hash + ']').addClass('selected');
 		        $('a[rel=ajax]').click(function () {
-		                var hash = this.href;
+				var hash = this.href;
 			        hash = hash.replace(/^.*#/, '');
 	 		        $.history.load(hash);	
 	 		        $('a[rel=ajax]').removeClass('selected');
@@ -43,182 +43,169 @@
 	 		        getPage();
 			        return false;
 	        	});	
-	});
-	
-	function pageload(hash) {
-		if (hash) getPage();    
-	}
-		
-	function getPage() {
-		var data = 'page=' + encodeURIComponent(document.location.hash);
-		$.ajax({
-			url: "loader.php",	
-			type: "GET",		
-			data: data,		
-			cache: false,
-			success: function (html) {	
-				$('#frontpage').hide();	
-				$('#bglogo').show();			
-				$('#content').html(html);
-				$('#innerpage').fadeIn('slow');		
-				bindLinks();
-		
-			}		
 		});
-	}
-	</script> 
+	
+		function pageload(hash) {
+			if (hash) getPage();    
+		}
+		
+		function getPage() {
+			var data = 'page=' + encodeURIComponent(document.location.hash.substring(1));
+			$.ajax({
+				url: "loader.php",	
+				type: "GET",		
+				data: data,		
+				cache: false,
+				success: function (html) {	
+					$('#frontpage').hide();	
+					$('#bglogo').show();			
+					$('#content').html(html);
+					$('#innerpage').fadeIn('slow');		
+					bindLinks();
+		
+				}		
+			});
+		}
+		</script> 
 		<title>
 			Tathva '11 | National Institute of Technology, Calicut
 		</title>
 	</head>
 	<body>
-
 	<div id = "wrapper">
-	
- 
- 	
   		<div id="oe_overlay" class="oe_overlay"></div>
-  		<div id = "topbar">
-   		
-    			<a href="">
+  		<div id = "topbar">   		
+    			<a href="index.php">
     			<div class="logo"></div>
     			</a>
     			<div class = "oe_wrapper">
-    			
-    			<ul id="oe_menu" class="oe_menu"> 
-    				<li><a href="#">Events</a>
-    				<div style="width:640px;">
-    				
-              <ul>
-                  <li class="oe_heading">Robotics</li>
-                  <li><a href="#transpoter" rel="ajax">Transporter</a></li>
-                  <li><a href="#leagueofmachines" rel="ajax">League of Machines</a></li>
-                  <li><a href="#miniaturemouse" rel="ajax">Miniature Mouse</a></li>
-                  <li><a href="#signalmaestro" rel="ajax">Signal Maestro</a></li>
-                  <li><a href="#robowars" rel="ajax">Robo Wars</a></li>
-                  <li><a href="#dirtrace" rel="ajax">Dirt Race</a></li>              
-              </ul>
-              <ul>
-                  <li class="oe_heading" >Mechanical</li>
-                  <li><a href="#cadquest" rel="ajax">Contraption</a></li>
-                  <li><a href="#incarnate" rel="ajax">Incarnate</a></li>
-                  <li><a href="#theoffroadmi" rel="ajax">The Off Road MI</a></li>
-                  <li><a href="#amphiboat" rel="ajax">Amphiboat</a></li>
-                  <li><a href="#waterrocket" rel="ajax">Water Rocket</a></li>
-              </ul>
-	      <ul>
-                  <li class="oe_heading">General</li>
-                  <li><a href="#blueprint" rel="ajax">Blueprint</a></li>
-                  <li><a href="#blitzkreig" rel="ajax">Blitzkreig</a></li>
-                  <li><a href="#losttreasureoftechila" rel="ajax">Lost Treasure of Techila</a></li>
-                  <li><a href="#idp" rel="ajax">Industry Defined Problems</a></li>
-              </ul>
-              
-              <ul>
-                  <li class="oe_heading">Online</li>
-                  <li><a href="#bullsnbears" rel="ajax">Bulls n Bears</a></li>
-                  <li><a href="#clueless" rel="ajax">Clueless</a></li>
-              </ul>
-              <ul>
-                  <li class="oe_heading">Management</li>
-                  <li><a href="#b-aptist" rel="ajax">B-Aptist</a></li>
-                  <li><a href="#tycoon" rel="ajax">Tycoon</a></li>
-              </ul>
-              <ul>
-                  <li class="oe_heading">Electrical</li>
-                  <li><a href="#mousedrive" rel="ajax">Mouse Drive</a></li>
-                  <li><a href="#coilgun" rel="ajax">Coil Gun</a></li>
-                  <li><a href="#puretricks" rel="ajax">Pure Tricks</a></li>
-              </ul>
-              <ul>
-                  <li class="oe_heading" >Electronics</li>
-                  <li><a href="#gsm" rel="ajax">GSM</a></li>
-                  <li><a href="#sonici" rel="ajax">Sonic i</a></li>
-                  <li><a href="#eracer" rel="ajax">E Racer</a></li>
-              </ul>
-              <ul>
-                  <li class="oe_heading">Civil</li>
-                  <li><a href="#erecthion" rel="ajax">eRECthion</a></li>
-                  <li><a href="#ageoffloatingempire" rel="ajax">Age of Floating Empire</a></li>
-                  <li><a href="#descartessquare" rel="ajax">Descartes Square</a></li>
-              </ul>
-	      <ul>
-                  <li class="oe_heading">Computer Science</li>
-                  <li><a href="#befunge" rel="ajax">Befunge</a></li>
-                  <li><a href="#koderkombat" rel="ajax">Koderkombat</a></li>
-                  <li><a href="#tuxofwar" rel="ajax">Tux of War</a></li>               
-              </ul>
-              <ul>
-                  <li class="oe_heading" >Chemical</li>
-                  <li><a href="#cheautic" rel="ajax">Che Autic</a></li>
-                  <li><a href="#interrepteur" rel="ajax">Interrupteur</a></li>
-              </ul>
-	      <ul>
-                  <li class="oe_heading">Biotechnology</li>
-		  <li><a href="#bizbioperzanta" rel="ajax">Bizbio Perzanta</a></li>
-                  <li><a href="#inquisitovirtuoso" rel="ajax">Inquisito Virtuoso</a></li>
-              </ul>
-    				</div>
-    				</li>
-    				<li><a href="#">Workshops</a>
-    				  <div style="width:320px;">
-    				    <ul>
-    				      <li class="oe_heading">General</li>
-    				      <li><a href="#astrophotography">Astro Photography</a></li>
-    				    </ul>
-    				    <ul>
-    				      <li class="oe_heading">Technical</li>
-    				      <li><a href="#rcplane">RC Plane</a></li>
-    				      <li><a href="#hackattack">Hack Attack</a></li>
-    				      <li><a href="#humanoidrobot">Bipedal Humanoid</a></li> 
-    				    </ul>
-    				  </div>
-    				</li>
-    				<li><a href="#">Exhibitions</a></li>
-    				<li><a href="#">Lectures</a>
-				<div style="width:160px;">
-    				    <ul>
-    				      <li><a href="#drtessythomas">Dr. Tessy Thomas</a></li>
-    				      <li><a href="#rajunarayanaswamy">Raju Narayana Swamy</a></li>
-
-    				    </ul>
-    				  </div>
-
-		
-				</li>
-     			</ul> <!-- oe_menu ???-->
+    				<ul id="oe_menu" class="oe_menu"> 
+    					<li><a href="#">Events</a>
+    						<div style="width:640px;">    				
+						      <ul>
+							  <li class="oe_heading">Robotics</li>
+							  <li><a href="#!transpoter" rel="ajax">Transporter</a></li>
+							  <li><a href="#!leagueofmachines" rel="ajax">League of Machines</a></li>
+							  <li><a href="#!miniaturemouse" rel="ajax">Miniature Mouse</a></li>
+							  <li><a href="#!signalmaestro" rel="ajax">Signal Maestro</a></li>
+							  <li><a href="#!robowars" rel="ajax">Robo Wars</a></li>
+							  <li><a href="#!dirtrace" rel="ajax">Dirt Race</a></li>              
+						      </ul>
+						      <ul>
+							  <li class="oe_heading" >Mechanical</li>
+							  <li><a href="#!cadquest" rel="ajax">Contraption</a></li>
+							  <li><a href="#!incarnate" rel="ajax">Incarnate</a></li>
+							  <li><a href="#!theoffroadmi" rel="ajax">The Off Road MI</a></li>
+							  <li><a href="#!amphiboat" rel="ajax">Amphiboat</a></li>
+							  <li><a href="#!waterrocket" rel="ajax">Water Rocket</a></li>
+						      </ul>
+						      <ul>
+							  <li class="oe_heading">General</li>
+							  <li><a href="#!blueprint" rel="ajax">Blueprint</a></li>
+							  <li><a href="#!blitzkreig" rel="ajax">Blitzkreig</a></li>
+							  <li><a href="#!losttreasureoftechila" rel="ajax">Lost Treasure of Techila</a></li>
+							  <li><a href="#!idp" rel="ajax">Industry Defined Problems</a></li>
+						      </ul>
+						      
+						      <ul>
+							  <li class="oe_heading">Online</li>
+							  <li><a href="#!bullsnbears" rel="ajax">Bulls n Bears</a></li>
+							  <li><a href="#!clueless" rel="ajax">Clueless</a></li>
+						      </ul>
+						      <ul>
+							  <li class="oe_heading">Management</li>
+							  <li><a href="#!b-aptist" rel="ajax">B-Aptist</a></li>
+							  <li><a href="#!tycoon" rel="ajax">Tycoon</a></li>
+						      </ul>
+						      <ul>
+							  <li class="oe_heading">Electrical</li>
+							  <li><a href="#!mousedrive" rel="ajax">Mouse Drive</a></li>
+							  <li><a href="#!coilgun" rel="ajax">Coil Gun</a></li>
+							  <li><a href="#!puretricks" rel="ajax">Pure Tricks</a></li>
+						      </ul>
+						      <ul>
+							  <li class="oe_heading" >Electronics</li>
+							  <li><a href="#!gsm" rel="ajax">GSM</a></li>
+							  <li><a href="#!sonici" rel="ajax">Sonic i</a></li>
+							  <li><a href="#!eracer" rel="ajax">E Racer</a></li>
+						      </ul>
+						      <ul>
+							  <li class="oe_heading">Civil</li>
+							  <li><a href="#!erecthion" rel="ajax">eRECthion</a></li>
+							  <li><a href="#!ageoffloatingempire" rel="ajax">Age of Floating Empire</a></li>
+							  <li><a href="#!descartessquare" rel="ajax">Descartes Square</a></li>
+						      </ul>
+						      <ul>
+							  <li class="oe_heading">Computer Science</li>
+							  <li><a href="#!befunge" rel="ajax">Befunge</a></li>
+							  <li><a href="#!koderkombat" rel="ajax">Koderkombat</a></li>
+							  <li><a href="#!tuxofwar" rel="ajax">Tux of War</a></li>               
+						      </ul>
+						      <ul>
+							  <li class="oe_heading" >Chemical</li>
+							  <li><a href="#!cheautic" rel="ajax">Che Autic</a></li>
+							  <li><a href="#!interrepteur" rel="ajax">Interrupteur</a></li>
+						      </ul>
+						      <ul>
+							  <li class="oe_heading">Biotechnology</li>
+							  <li><a href="#!bizbioperzanta" rel="ajax">Bizbio Perzanta</a></li>
+							  <li><a href="#!inquisitovirtuoso" rel="ajax">Inquisito Virtuoso</a></li>
+						      </ul>
+    						</div>
+    					</li>
+    					<li><a href="#">Workshops</a>
+    				  		<div style="width:320px;">
+		    					<ul>
+		    				      		<li class="oe_heading">General</li>
+		    				      		<li><a href="#!astrophotography">Astro Photography</a></li>
+		    				    	</ul>
+		    				    	<ul>
+			    				      	<li class="oe_heading">Technical</li>
+			    				      	<li><a href="#!rcplane">RC Plane</a></li>
+			    				      	<li><a href="#!hackattack">Hack Attack</a></li>
+			    				      	<li><a href="#!humanoidrobot">Bipedal Humanoid</a></li> 
+			    				</ul>
+    				  		</div>
+    					</li>
+    					<li><a href="#">Exhibitions</a></li>
+    					<li><a href="#">Lectures</a>
+						<div style="width:160px;">
+    				    			<ul>
+    				      				<li><a href="#!drtessythomas">Dr. Tessy Thomas</a></li>
+    				      				<li><a href="#!rajunarayanaswamy">Raju Narayana Swamy</a></li>
+    				    			</ul>
+    				  		</div>
+					</li>
+     				</ul> <!-- oe_menu ???-->
     			</div>  
-    				<div id = "searchcontainer">
-    			<form id="searchform" action="search.php" method="get">
-      			<div class="searchboxwrapper">
-      		  	<input class="searchbox" name="q" type="text" autocomplete="off"/> 
-      			</div>
-      			<input id="searchbutton" type="submit" />
-    			</form>
+    			<div id = "searchcontainer">
+    				<form id="searchform" action="search.php" method="get">
+      					<div class="searchboxwrapper">
+      		  				<input class="searchbox" name="q" type="text" autocomplete="off"/> 
+      					</div>
+      					<input id="searchbutton" type="submit" />
+    				</form>
     			</div>
     			<div id = "loginlinkscontainer">
-    			<ul id="loginlinks">
-    				<li><a href="">Register</a></li>
-				
-    			</ul>
+	    			<ul id="loginlinks">
+	    				<li><a href="">Register</a></li>				
+	    			</ul>
     			</div>	
-  		</div>
-  		  		
+  		</div>  		  		
   		<div id = "sidebar">
-  		<ul>
-  		<li><a href="#info" ><img class ="sbicon" title="Information" src="styles/images/sbinfo.png"/></a></li>
-  		<li><a href="#schedule" ><img class ="sbicon" title="Schedule" src="styles/images/sbschedule.png"/></a></li>
-  		<li><a href="#gallery" ><img class ="sbicon" title="Gallery" src="styles/images/sbgallery.png"/></a></li>
-  		<li><a href="#sponsors" ><img class ="sbicon" title="Sponsorship" src="styles/images/sbsponsorship.png"/></a></li>
-  		<li><a href="#feedback" ><img class ="sbicon" title="Feedback" src="styles/images/sbfeedback.png"/></a></li>
-  		</ul>
-  		
+	  		<ul>
+		  		<li><a href="#!info" ><img class ="sbicon" title="Information" src="styles/images/sbinfo.png"/></a></li>
+		  		<li><a href="#!schedule" ><img class ="sbicon" title="Schedule" src="styles/images/sbschedule.png"/></a></li>
+		  		<li><a href="#!gallery" ><img class ="sbicon" title="Gallery" src="styles/images/sbgallery.png"/></a></li>
+		  		<li><a href="#!sponsors" ><img class ="sbicon" title="Sponsorship" src="styles/images/sbsponsorship.png"/></a></li>
+		  		<li><a href="#!feedback" ><img class ="sbicon" title="Feedback" src="styles/images/sbfeedback.png"/></a></li>
+	  		</ul>
   		</div>
 
   		<div id = "frontpage">
-  		<div id = "headerlogo">
-  		 </div>
-		<div id="sponsor">
+	  		<div id = "headerlogo">
+	  		</div>
+			<div id="sponsor">
 			<div id="sponsslider"> 
  	 <ul> 
  	  <li><img src="styles/images/sponsors/gasotech.png" alt=""/></li>
@@ -230,6 +217,7 @@
  	 </ul> 
  	 </div> 
 		</div>
+		<?php if(!isset($_GET['_escaped_fragment_'])): ?>
 		 <div id="slider">
 		   <ul id="panels">
 		   <li id='panel0'>
@@ -311,15 +299,14 @@
 		   </div>
         			
 		 </div>
-		
+			   <?php endif; ?>
   		</div>
-  		
-  		<div id = "innerpage">
+  		<div id="innerpage">
   		                <div id="content">
-                                <!-- Ajax Content -->
+	                  		<?php if(isset($_GET['_escaped_fragment_'])) echo file_get_contents('http://localhost/Tathva--11-Website/loader.php?page='.$_GET['_escaped_fragment_']); ?>
+  		                	<!-- Ajax Content -->
                                 </div>         
                 </div> 
-  		
   		<div id = "updatebar">
   		  <div id = "updates">This is the updates bar and all updates are going to come here</div>
   		  <div id = "contactsbutton">Contacts [+]</div>
--- loader.php
+++ loader.php
@@ -1,13 +1,15 @@
 <?php
- require_once 'config.php'; 
+require_once 'config.php'; 
 $con = mysql_connect($host, $db_user, $db_password);
 if (!$con) {
 	die('Could not connect: ' . mysql_error());
 	}
 $db = mysql_select_db($db_name, $con);
-$sql = "SELECT article_content FROM articles WHERE article_hash='".$_GET['page']."'";
+$sql = "SELECT article_content FROM articles WHERE article_hash='#".$_GET['page']."'";
 $result = mysql_query($sql,$con) or die("no content") ;
 $row = mysql_fetch_array($result);
-echo $row['article_content'];
-
+if (!($row))
+	echo '404';
+else
+	echo $row['article_content'];
 ?>
