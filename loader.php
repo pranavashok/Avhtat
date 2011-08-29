44895f6e911637a3ac9bda8b82eb1b05f8147683
Shamil CM <shamil.cm@gmail.com>
2011-08-29 13:44:15 +0530

    added info page

--- loader.php
+++ loader.php
@@ -42,8 +42,78 @@ echo '<div id="stylized" class="myform">
 }
 else if($_GET['page']=="info")
 {
-echo '<div id="ititle">The Journey</div>';
+echo '<div id="ititle">Information</div>
+<div id="imenu">
+<ul class = "ilinks">
+<li id="1">Registration</li>
+<li id="2">Hospitality</li>
+<li id="3">Deregistration</li>
+</ul>
+</div>
+
+<div id="imcs_container">
+<div class="icustomScrollBox">
+<div class="icontainer">
+<div class="icontent">
+<div id="isection0" class="isection">
+<ul>
+<li>A Valid Tathva ID is compulsory in order to get      accommodation facilities and for taking part in any event.</li>
+<li>Participants should bring valid college ID cards as a      proof for identity.</li>
+<li>Events registered online should be confirmed at the registration      desk.</li>
+<li>Tathva ID and College ID card is must for attending      Tathva nights.</li>
+<li>For team events, event team captain has to register      along with the team members’ Tathva ID.</li>
+<li>The registration fee for Tathva’11 is Rs 250 and Rs 50      as refundable caution deposit. (Rs 250 + 50 refundable)</li>
+<li>A separate fee will be collected from those who avail      accommodation before the days of Tathva.</li>
+<li>For registering additional events, participants should      register at the registration desk.</li>
+<li>Accommodation will be provided usually to a group of 3      or 4. Please try to come as in a group to make procedures simpler.</li>
+<li>Participants are bound to repay the losses they make      during their stay here. </li>
+</ul></div>
+
+<div id="isection1" class="isection">
+<ul>
+<li>Participants reaching the college should move on to the      Registration Desk at the Main Building(MB).</li>
+<li>The participant’s details will be cross checked at the      first counter.</li>
+<li>Participants who have not done the online registration      might have to stand in a queue to get themselves registered.</li>
+<li>After this, the participant should move to the cash      counter where they have to pay the registration fee and they will be given      the Tathva ID card. Please cross check the name and Tathva ID card given      there itself.</li>
+<li>You will be given a payment receipt.</li>
+</ul>
+</div>
+
+<div id="isection2" class="isection">
+<ul>
+<li>Those who require accommodation facility should move to      the Hospitality desk.</li>
+<li>Please show the receipt of payment obtained from the      registration desk.</li>
+<li>Try to come in groups of 3 or 4. One of the      participants in each group will be made as the hospitality team captain.</li>
+<li><strong>THE EVENT TEAM CAPTAIN NEED NOT BE THE SAME AS THE      HOSPITALITY TEAM CAPTAIN.</strong></li>
+<li>All the required utilities for stay for the entire      group will be handed over to the respective team captain.</li>
+<li>It will be the team captain\'s duty to return the      utilities without damage.</li>
+</ul>
+
+<br/>
+</div>
+
+<div id="isection3" class="isection">
+<ul>
+<li>Before deregistering our executives will check for the      damage in the utilities given to you during stay.</li>
+<li>If the team captain leaves before the other team      members please inform us and re edit the team captain.</li>
+<li>After getting cleared from the hospitality desk move on      to the registration desk.</li>
+<li>Here the hospitality team captain can collect the      caution deposit of his/her team.</li>
+<li>Accommodation receipt must be produced during      deregistration. </li>
+<li>The prizes for team events will be given only to the      event team captain.</li>
+</ul>
+</div>
+
+</div><!--icontent-->
+</div><!--icontainer-->
+<div class="idragger_container">
+<div class="idragger"></div>
+</div>
+<div id="igrad"></div>
+</div><!--icustomScrollBox-->
+<a href="#" class="iscrollUpBtn"></a><a href="#" class="iscrollDownBtn"></a>
+</div> <!--imcs_container-->';
 }
+
 else if($_GET['page']=="schedule")
 {
 echo '<div id="ititle">Schedule</div><div id="imcs_container">
