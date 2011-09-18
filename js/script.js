9cd1e5d77f970f6591aee8b9dead10231c962c8c
Sachin Krishnan T V <sachu92@gmail.com>
2011-09-19 02:19:08 +0530

    search loader

--- js/script.js
+++ js/script.js
@@ -211,9 +211,11 @@ function lookup(inputString) {
 		if(inputString.length == 0) {
 			$('#suggestions').fadeOut(); // Hide the suggestions box
 		} else {
-			$.post("rpc.php", {queryString: ""+inputString+""}, function(data) { // Do an AJAX call
 			$('#suggestions').fadeIn(); // Show the suggestions box
+			$("#s-loader").show();
+			$.post("rpc.php", {queryString: ""+inputString+""}, function(data) { // Do an AJAX call
 				$('#suggestions').html(data); // Fill the suggestions box
+				$("#s-loader").hide();
       			});
    		}
 	}
